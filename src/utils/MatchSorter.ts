24833eaa2103bae8d359b966192b58d865aa6150
Kate Yefits <kate.yefits@gmail.com>
2025-01-25 22:02:51 +0100

    Added MatchSorter algorithm

--- src/utils/MatchSorter.ts
+++ src/utils/MatchSorter.ts
@@ -1,5 +1,8 @@
 import { Match } from "../types/Match";
 
 export const sortMatches = (matches: Match[]): Match[] => {
-    return matches
+    return [...matches].sort((a, b) => {
+      const totalScoreDiff = (b.homeScore + b.awayScore) - (a.homeScore + a.awayScore);
+      return totalScoreDiff !== 0 ? totalScoreDiff : b.startTime - a.startTime;
+    });
   };
